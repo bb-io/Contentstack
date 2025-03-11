7e6ffbcbf53162e4ddfb0c266c6950d70c567fcd
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-11 20:39:27 +0200

    Modified action List content types to Search content types

---
+++ Apps.Contentstack/Models/Request/ContentType/ContentTypesRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.Contentstack.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Contentstack.Models.Request.ContentType
+{
+    public  class ContentTypesRequest
+    {
+        [Display("Content type ID")]
+        [DataSource(typeof(ContentTypeDataHandler))]
+        public IEnumerable<string>? ContentTypeId { get; set; }
+    }
+}
