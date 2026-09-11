bbc2030eccb947c4c0e2d683a61d3ac0a360d786
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-11 11:15:55 +0200

    improve branch check in ContentstackRequest

--- Apps.Contentstack/Api/ContentstackRequest.cs
+++ Apps.Contentstack/Api/ContentstackRequest.cs
@@ -16,8 +16,8 @@ public class ContentstackRequest(string resource, Method method, IEnumerable<Aut
         this.AddHeader("api_key", credsList.Get(CredsNames.StackApiKey).Value);
         this.AddHeader("authorization", credsList.Get(CredsNames.ManagementToken).Value);
 
-        string? connectionType = credsList.FirstOrDefault(x => x.KeyName == CredsNames.ConnectionType)?.Value;
-        if (connectionType == ConnectionType.BranchScopedApiKey)
-            this.AddHeader("branch", credsList.Get(CredsNames.BranchName).Value);
+        string? branch = credsList.FirstOrDefault(x => x.KeyName == CredsNames.BranchName)?.Value;
+        if (!string.IsNullOrWhiteSpace(branch))
+            this.AddHeader("branch", branch);
     }
 }
